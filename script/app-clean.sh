0dbd134a70ac31c188c43c51337f6537e59025f6
GiuseppeTT <giu.tinti@gmail.com>
2022-12-30 01:55:54 +0000

    Improve script

--- script/app-clean.sh
+++ script/app-clean.sh
@@ -4,7 +4,7 @@
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
 # Clean other folders
-rm -rf .mypy_cache
-rm -rf .pytest_cache
-rm -rf .terraform
-rm -rf .venv
+rm -rf .mypy_cache/
+rm -rf .pytest_cache/
+rm -rf .terraform/
+rm -rf .venv/
